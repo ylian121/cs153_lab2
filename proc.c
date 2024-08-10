e5cc8a11409ed093827a47ec1c109134d107e272
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-08-10 07:23:47 +0000

    task 1 aging

--- proc.c
+++ proc.c
@@ -409,62 +409,21 @@ scheduler(void)
       if(p->state!= RUNNABLE)
         continue;
 
-      if(p->priority_val == h_prior_val){
-        // Switch to chosen process.  It is the process's job
-        // to release ptable.lock and then reacquire it
-        // before jumping back to us.
-        c->proc=p;
-        switchuvm(p);
-        p->state=RUNNING;
-
-        swtch(&(c->scheduler), p->context);
-        switchkvm();
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc=0;
-      }
-    }
-  
-    release(&ptable.lock);
-
-  }
-
-
-  //original default simple round robin scheduler
-  /*
-  struct proc *p;
-  struct cpu *c = mycpu();
-  c->proc = 0;
-  
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
-
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
+      if(p == priority_process){
         continue;
+      }
 
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+      //decrease proc that hasn't run yet
+      if(p->priority_val > 0) {
+        (p->priority_val)--;
+      }
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
     }
+  
     release(&ptable.lock);
 
   }
-  */
+
 }
 
 // Enter scheduler.  Must hold only ptable.lock
