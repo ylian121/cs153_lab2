6219794d3d013f3c8817d4d3b54fe77217182328
Yongyan Liang <ylian121@xe-09.cs.ucr.edu>
2024-08-10 06:14:11 +0000

    task 0 priority scheduler

--- proc.c
+++ proc.c
@@ -331,6 +331,62 @@ wait(void)
 void
 scheduler(void)
 {
+
+  struct proc *p;
+  struct cpu *c = mycpu();
+  c->proc = 0;
+
+  for(;;){
+    // Enable interrupts on this processor.
+    sti();
+    //declare priority value variable
+    //range: 0 to 30 then highest priority can't pass 31
+    int h_prior_val = 31;
+
+    // Loop over process table looking for process to run.
+    acquire(&ptable.lock);
+
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      //can only run states that are runnable
+      if(p->state!= RUNNABLE)
+        continue;
+      
+      if(p->priority_val < h_prior_val){
+        h_prior_val = p->priority_val;
+      }
+    }
+
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+    {
+      //can only run states that are runnable
+      if(p->state!= RUNNABLE)
+        continue;
+
+      if(p->priority_val == h_prior_val){
+        // Switch to chosen process.  It is the process's job
+        // to release ptable.lock and then reacquire it
+        // before jumping back to us.
+        c->proc=p;
+        switchuvm(p);
+        p->state=RUNNING;
+
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc=0;
+      }
+    }
+  
+    release(&ptable.lock);
+
+  }
+
+
+  //original default simple round robin scheduler
+  /*
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
@@ -362,6 +418,7 @@ scheduler(void)
     release(&ptable.lock);
 
   }
+  */
 }
 
 // Enter scheduler.  Must hold only ptable.lock
